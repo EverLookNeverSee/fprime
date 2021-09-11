aa7d516d5fe2e901803cbba65d4ca82b88e6232e
L <82917144+l198881@users.noreply.github.com>
2021-09-11 18:07:38 -0300

    Update Comm.py

--- Comm.py
+++ Comm.py
@@ -1,13 +1,1 @@
-Command office > fpp-depend --help
-fpp-depend v1.0.0 commit c27c1a7d9b611fe20d276f140ba67de257a94b1c
-Usage: fpp-depend [options] [file ...]
-
-  -d, --direct <file>     write direct dependencies to file
-  -g, --generated <file>  write names of generated files to file
-  -i, --included <file>   write included dependencies to file
-  -m, --missing <file>    write missing dependencies to file
-  -h, --help              print this message and exit
-  file ...                input files
-
-import onudatacloud
-        
+Done.
