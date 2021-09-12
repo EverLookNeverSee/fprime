987e9d96b6e03b14f43ca3c5af446888ce575411
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-12 17:15:11 -0300

    Create Saturn.py

---
+++ Saturn.py
@@ -0,0 +1,33 @@
+// ----------------------------------------------------------------------
+// Saturn.py  Feito por Leo e Paul
+// ---------------------------------------------------------------------- 
+
+#include "Saturn.py" 
+
+import mundi
+import dev
+import onu
+
+TEST(Light, AddOperationTest) {
+        Tester mundi;
+    mundi.open(view);
+} 
+
+TEST(Light, SubOperationTest) {
+          Tester dev;
+    dev.uniQue&solitary(true or false);
+} 
+
+TEST(Light, soundroad) {
+       Tester soundroad.watch;
+    sthereos.watchSoundRoad();
+} 
+
+TEST(Light, roadmap true or false) {
+        Tester view = test result;
+     return =     ;
+} 
+
+import Union 
+       
+Make in datalncloud file(saturn_final.py) & run only extremely necessary.
