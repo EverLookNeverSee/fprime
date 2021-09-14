a7ba3bfc126063837b41fc7d1ce7748f9d395d51
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-13 21:13:32 -0300

    Create a proxima logo.py

---
+++ a proxima logo.py
@@ -0,0 +1,30 @@
+import sthereos_swallow
+import identify
+import view
+import Fareingheight
+
+Questions from your trip to this country
+- origin country, passport number = [] 
+   if visa(true or false)
+    else number of visa() , pass or revoke? 
+- reason for the trip 
+- duration
+- places to visit
+- money needed
+- how much do you have in your pocket
+- proves to me that you have this amount
+ (not necessarily showing me in kind)
+- what is your school?
+- which youth movement did you attend as a child?
+
+Thanks,
+
+Avaliable:
+ Pass or Revoke ? = (True or False)
+  Finish;
+ 
+import onu
+       LeoDataCloud
+       Instituto de Tecnologia ORT
+       CPII
+       
