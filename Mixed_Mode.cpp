902461ba58590f359da01d8a5a425cb86b0463c2
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-25 02:13:30 -0300

    Update Mixed_Mode.cpp

--- Mixed_Mode.cpp
+++ Mixed_Mode.cpp
@@ -1,11 +1,1 @@
-#include "Mixed_Mode.h"
-#ifndef MIXED_MODE_MULTIPLY_HPP
-#define MIXED_MODE_MULTIPLY_HPP
 
-extern "C"
-{
-  __declspec(dllexport) int __stdcall mixed_mode_multiply(int a, int b) {
-    return a * b;
-  }
-}
-#endif
